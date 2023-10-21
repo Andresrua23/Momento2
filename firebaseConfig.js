3a770e1ef3af124840840901b78c20042e1746c6
Andresrua23 <andresrua987@gmail.com>
2023-10-21 18:42:45 -0500

    Andresrua23
    todo ok

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC7gBaglUikQ1WVsrgGWfkToYgCqYVr6No",
+  authDomain: "loginrn-32cf6.firebaseapp.com",
+  projectId: "loginrn-32cf6",
+  storageBucket: "loginrn-32cf6.appspot.com",
+  messagingSenderId: "443998481001",
+  appId: "1:443998481001:web:1a7db5e1c1ed97031350f8"
+};
